5003b2689419124671439d24f501da6af427d526
pvnick <pvnick@yandex.ru>
2023-10-19 04:26:07 +0700

    добавил забытый коммит к функции добавления отзыва, теперь обновление рейтинга работает, урашеньки

--- db_functions.py
+++ db_functions.py
@@ -41,7 +41,7 @@ def write_user_on_start(message):
     conn.close()
 
 
-def add_feedback(minor_feedback):
+def add_feedback(limit_to_six):
     '''функция добавления отзыва в БД'''
 
     print('функция добавления отызва запустилась')
@@ -52,11 +52,11 @@ def add_feedback(minor_feedback):
                    product_name VARCHAR(255), username VARCHAR(150),
                    feedback TEXT, rating INT
                    )''')
-    print(minor_feedback)
-    product_name = minor_feedback[0][1] + ' ' + minor_feedback[0][0]
+    product_name = limit_to_six[0][1] + ' ' + limit_to_six[0][0]
+    feedback_text = limit_to_six[0][3]
     print(product_name)
-    if not search_same_records(product_name):
-        for i in minor_feedback:
+    if not search_same_records(product_name, feedback_text):
+        for i in limit_to_six:
             data = (product_name, i[2], i[3], 0)
             print('data -', data)
             cursor.execute(
@@ -70,15 +70,31 @@ def add_feedback(minor_feedback):
     conn.close()
 
 
-def search_same_records(record):
+def search_same_records(record, feedback_text):
     '''функция ищет записи-дубликаты в БД'''
 
     conn = sqlite3.connect('db_wb.sqlite3')
     cursor = conn.cursor()
     # такой способ форматирования строки, т.к. f-строка не работает с sqlite
-    cursor.execute('SELECT product_name FROM wb_feedback WHERE product_name = "%s"' % record)
+    cursor.execute('SELECT product_name FROM wb_feedback WHERE product_name = "%s" and feedback = "%s"' % (record, feedback_text))
     res = cursor.fetchall()
-    print('результат выборки записей-дубликатов из БД -', res)
     cursor.close()
     conn.close()
     return res
+
+
+def vote_for_feedback(elem):
+    '''функция обновляет рейтинг, увеличивая его на единицу'''
+
+    print('вошли в функцию голосования за отзыв - vote_for_feedback')
+    product_name = elem[1] + ' ' + elem[0]
+    feedback_text = elem[3]
+    conn = sqlite3.connect('db_wb.sqlite3')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE wb_feedback SET rating=rating+1 WHERE product_name = "%s" and feedback = "%s"' % (product_name, feedback_text))
+    res = cursor.fetchone()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print('обновили рейтинг отзыва с продуктом и текстом: ', product_name, feedback_text, sep='\n')
+    print(res)
